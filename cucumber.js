d56d3b376f64290fbe71775549fe4d8dcc6c27de
amanda <chenyuechong@gmail.com>
2025-01-26 12:07:08 +1300

    update report path

--- cucumber.js
+++ cucumber.js
@@ -1,4 +1,4 @@
 module.exports = {
-    default: `--require-module ts-node/register --require test/step-definitions/*.ts --require src/hooks/hooks.ts --format progress-bar --format json:cucumber-report.json test/features/*.feature`
+    default: `--require-module ts-node/register --require test/step-definitions/*.ts --require src/hooks/hooks.ts --format progress-bar --format json:reports/cucumber-report.json test/features/*.feature`
   };
   
